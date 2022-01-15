3c2825c0b769107261beafa972069670f887e8b6
lavender-qaq <1254297317@qq.com>
2022-01-15 21:55:02 +0800

    add C++

---
+++ build/CMakeFiles/algo_main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algo_main.dir/Algorithm/main.cpp.o"
+  "CMakeFiles/algo_main.dir/Algorithm/main.cpp.o.d"
+  "algo_main"
+  "algo_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algo_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/Lavender/Desktop/Go项目/Go-for-Acwing
+# Build directory: /Users/Lavender/Desktop/Go项目/Go-for-Acwing/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

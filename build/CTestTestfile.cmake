06c83340dfbc07bcc3bab62801d0e669d1dbf2ba
lavender-qaq <1254297317@qq.com>
2022-01-15 22:05:04 +0800

    ignore build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Lavender/Desktop/Go项目/Go-for-Acwing
-# Build directory: /Users/Lavender/Desktop/Go项目/Go-for-Acwing/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
